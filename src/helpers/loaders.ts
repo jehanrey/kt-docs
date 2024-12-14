432445afbac74d9f33083b81879f741e3c4b7eaf
Jehan Reyes <yehan.reyes@gmail.com>
2024-12-14 23:29:37 +0800

    fix default authentication page

--- src/helpers/loaders.ts
+++ src/helpers/loaders.ts
@@ -4,18 +4,20 @@ import { categories } from '@/wiki/authentication/constants'
 
 import { groupBy } from './groupBy'
 
+export const getSortedAuthCollection = async () => {
+  return (await getCollection('authentication')).sort((a, b) => {
+    const categoryIndex = (category: string) =>
+      categories.findIndex((c) => c === category)
+    if (categoryIndex(a.data.category) > categoryIndex(b.data.category))
+      return 1
+    if (categoryIndex(b.data.category) > categoryIndex(a.data.category))
+      return -1
+    return a.data.order - b.data.order
+  })
+}
+
 export const getCategorizedAuthCollection = async () => {
-  const sortedAuthCollection = (await getCollection('authentication')).sort(
-    (a, b) => {
-      const categoryIndex = (category: string) =>
-        categories.findIndex((c) => c === category)
-      if (categoryIndex(a.data.category) > categoryIndex(b.data.category))
-        return 1
-      if (categoryIndex(b.data.category) > categoryIndex(a.data.category))
-        return -1
-      return a.data.order - b.data.order
-    },
-  )
+  const sortedAuthCollection = await getSortedAuthCollection()
   return groupBy(
     sortedAuthCollection.map(({ id, data }) => ({ id, ...data })),
     'category',
