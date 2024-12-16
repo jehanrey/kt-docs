e9e156a5486590e11a529e6a2efd5695deab7e68
rj.reyes <rj.reyes@samsung.com>
2024-12-16 15:55:40 +0800

    adjust wpm to filipino average

--- src/lib/markdown/remark-reading-time.mjs
+++ src/lib/markdown/remark-reading-time.mjs
@@ -4,7 +4,7 @@ import getReadingTime from 'reading-time'
 export function remarkReadingTime() {
   return function (tree, file) {
     const textOnPage = toString(tree)
-    const readingTime = getReadingTime(textOnPage)
+    const readingTime = getReadingTime(textOnPage, { wordsPerMinute: 130 })
     file.data.astro.frontmatter.minutesRead = readingTime.text
   }
 }
